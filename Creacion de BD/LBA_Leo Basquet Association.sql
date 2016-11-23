7db210390f6442422e6c24305bf902882c0e4d94
Alexmaister <aortizurita@gmail.com>
2016-11-23 01:57:57 +0100

    Terminado LBA

--- Creacion de BD/LBA_Leo Basquet Association.sql
+++ Creacion de BD/LBA_Leo Basquet Association.sql
@@ -19,7 +19,10 @@ CREATE TABLE LBA_Jugadores
 		Dorsal TINYINT NOT NULL,
 		Posicion CHAR(1) NULL,
 		Nacionalidad CHAR(3),
-		CONSTRAINT FK_LBA_Paises FOREIGN KEY (Nacionalidad) REFERENCES LBA_Paises (ID)
+		CONSTRAINT FK_LBA_Paises FOREIGN KEY (Nacionalidad) REFERENCES LBA_Paises (ID),
+		CONSTRAINT CK_Edad CHECK (CURRENT_TIMESTAMP-FechaNac BETWEEN 15 AND 50),
+		CONSTRAINT CK_NumeroDorsal CHECK (Dorsal BETWEEN 0 AND 99),
+		CONSTRAINT CK_Posiciones CHECK (Posicion IN ('B','A','P','E','L'))
 	)
 
 CREATE TABLE LBA_Canchas
@@ -38,6 +41,7 @@ CREATE TABLE LBA_Equipos
 		Localidad VARCHAR(25) NOT NULL,
 		ID_Cancha TINYINT,
 		CONSTRAINT FK_LBA_Canchas FOREIGN KEY (ID_Cancha) REFERENCES LBA_Canchas (ID),
+		CONSTRAINT CK_AñoFundacion CHECK (Fecha_Fundacion < CURRENT_TIMESTAMP)
 	)
 
 CREATE TABLE LBA_Partidos
@@ -46,8 +50,8 @@ CREATE TABLE LBA_Partidos
 		Fecha_Hora SMALLDATETIME NOT NULL,
 		ID_Local CHAR(3) NOT NULL,
 		ID_Visitante CHAR(3) NOT NULL,
-		Puntos_Local TINYINT,
-		Puntos_Visitante TINYINT,
+		Puntos_Local TINYINT DEFAULT NULL,
+		Puntos_Visitante TINYINT DEFAULT NULL,
 		CONSTRAINT FK_LBA_EquipoLocal FOREIGN KEY (ID_Local) REFERENCES LBA_Equipos(CODIGO),
 		CONSTRAINT FK_LBA_EquipoVis FOREIGN KEY (ID_Visitante) REFERENCES LBA_Equipos(CODIGO)
 	)
@@ -65,6 +69,10 @@ CREATE TABLE LBA_Juega
 		Puntos3 TINYINT,
 		Personales TINYINT,
 		CONSTRAINT FK_LBA_Jugadores FOREIGN KEY (Licencia) REFERENCES LBA_Jugadores (Licencia),
-		CONSTRAINT FK_LBA_Partidos FOREIGN KEY (ID_Partido) references LBA_Partidos (ID)
+		CONSTRAINT FK_LBA_Partidos FOREIGN KEY (ID_Partido) references LBA_Partidos (ID),
+		CONSTRAINT CK_TiempoDeJuego CHECK (Minutos >0 AND Minutos<40),
+		CONSTRAINT CK_TirosAciertos1 CHECK (Puntos1<=Tiros1),
+		CONSTRAINT CK_TirosAciertos2 CHECK (Puntos2/2<=Tiros2),
+		CONSTRAINT CK_TirosAciertos3 CHECK (Puntos3/3<=Tiros3)
 	)
 
