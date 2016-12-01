6948637520a503112b429561c97bac9efac8b262
Alexmaister <aortizurita@gmail.com>
2016-12-01 01:37:59 +0100

    Terminado Modifica LeoBikes

--- Modificaciones/Modificaciones de modelo.sql
+++ Modificaciones/Modificaciones de modelo.sql
@@ -15,3 +15,21 @@ CREATE TABLE LB_Avisos
 ALTER TABLE LB_Productos ADD CONSTRAINT DF_StockActual DEFAULT  0 FOR Stock_Actual
 ALTER TABLE LB_Productos ADD CONSTRAINT DF_StockMinimo DEFAULT  0 FOR Stock_Minimo
 
+ALTER TABLE LB_Clientes ADD CONSTRAINT DF_SaldoPuntosxDefecto DEFAULT 0 FOR SaldoPuntos
+ALTER TABLE LB_Clientes WITH NOCHECK ADD CONSTRAINT CK_SaldoPuntosPositivo CHECK (SaldoPuntos >=0)
+
+ALTER TABLE LB_Avisos ADD FechaCrea datetime 
+ALTER TABLE LB_Avisos ADD FechaActual datetime DEFAULT CURRENT_TIMESTAMP
+ALTER TABLE LB_Avisos ADD CONSTRAINT DF_FechaActual DEFAULT CURRENT_TIMESTAMP FOR FechaCrea
+
+ALTER TABLE LB_Avisos DROP FK_Productos_Avisos
+ALTER TABLE LB_Avisos ADD CONSTRAINT FK_Productos_Avisos FOREIGN KEY (ID) REFERENCES LB_Productos (Codigo) 
+ON UPDATE CASCADE ON DELETE CASCADE
+
+ALTER TABLE LB_Avisos ADD CONSTRAINT CK_FechaMayor CHECK (FechaActual>FechaCrea)
+
+ALTER TABLE LB_Clientes ADD CONSTRAINT CK_Descuento50x100 CHECK (Descuento>=0 and Descuento <=50)
+ALTER TABLE LB_Productos ADD Margen AS Stock_Actual-Stock_Minimo
+
+
+
