9d416eecb6866a00c0a979ab2c4137bba2ea7e76
Alexmaister <aortizurita@gmail.com>
2017-02-20 14:02:02 +0100

    New boletin

---
+++ Subconsultas/Boletin 9.2.sql
@@ -0,0 +1,54 @@
+--Unidad 9. Consultas elaboradas en SQL Server
+-- NorthWind.
+go
+use Northwind
+go
+--PROBLEMA
+--1. Número de clientes de cada país.
+select COUNT(CustomerID)as[Número de clientes],Country from Customers group by Country
+
+--2. Número de clientes diferentes que compran cada producto.
+select distinct COUNT(C.CustomerID)as[Número de clientes],P.ProductName from Customers as C
+join Orders as O
+on C.CustomerID=O.CustomerID
+join [Order Details] as OD
+on O.OrderID=OD.OrderID
+join Products as P
+on OD.ProductID=P.ProductID
+group by P.ProductID,P.ProductName
+
+--3. Número de países diferentes en los que se vende cada producto.
+
+
+--4. Empleados que han vendido alguna vez “Gudbrandsdalsost”, “Lakkalikööri”,
+--“Tourtière” o “Boston Crab Meat”.
+--5. Empleados que no han vendido nunca “Chartreuse verte” ni “Ravioli Angelo”.
+select FirstName,LastName from Employees
+except(
+select  E.FirstName,E.LastName from Employees as E
+join Orders as O
+on E.EmployeeID=O.EmployeeID
+join [Order Details] as OD
+on O.OrderID=OD.OrderID
+join Products as P
+on OD.ProductID=P.ProductID
+where P.ProductName='Chartreuse verte' and P.ProductName='Ravioli Angelo'
+)
+--6. Número de unidades de cada categoría de producto que ha vendido cada
+--empleado.
+--7. Total de ventas (US$) de cada categoría en el año 97.
+--8. Productos que han comprado más de un cliente del mismo país, indicando el
+--nombre del producto, el país y el número de clientes distintos de ese país que
+--lo han comprado.
+--9. Total de ventas (US$) en cada país cada año.
+--10. Producto superventas de cada año, indicando año, nombre del producto,
+--categoría y cifra total de ventas.
+--11. Cifra de ventas de cada producto en el año 97 y su aumento o disminución
+--respecto al año anterior en US $ y en %.
+--12. Mejor cliente (el que más nos compra) de cada país.
+--13. Número de productos diferentes que nos compra cada cliente.
+--14. Clientes que nos compran más de cinco productos diferentes.
+--15. Vendedores que han vendido una mayor cantidad que la media en US $ en el
+--año 97.
+--16. Empleados que hayan aumentado su cifra de ventas más de un 10% entre dos
+--años consecutivos, indicando el año en que se produjo el aumento.
