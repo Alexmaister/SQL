3e472f3cb91042e408018431e2da4042eaf6df45
Alexmaister <aortizurita@gmail.com>
2017-02-28 22:47:49 +0100

    hnfnn
    dfn

--- Subconsultas/Boletin 9.2.sql
+++ Subconsultas/Boletin 9.2.sql
@@ -20,9 +20,24 @@ group by P.ProductID,P.ProductName
 --3. Nï¿½mero de paï¿½ses diferentes en los que se vende cada producto.
 
 
+<<<<<<< Updated upstream
 --4. Empleados que han vendido alguna vez ï¿½Gudbrandsdalsostï¿½, ï¿½Lakkalikï¿½ï¿½riï¿½,
 --ï¿½Tourtiï¿½reï¿½ o ï¿½Boston Crab Meatï¿½.
 --5. Empleados que no han vendido nunca ï¿½Chartreuse verteï¿½ ni ï¿½Ravioli Angeloï¿½.
+=======
+--4. Empleados que han vendido alguna vez “Gudbrandsdalsost”, “Lakkalikööri”,
+--“Tourtière” o “Boston Crab Meat”.
+select * from [Orders]
+select * from Employees as E
+inner join Orders as O
+on E.EmployeeID=O.EmployeeID
+inner join [Order Details] as OD
+on O.OrderID=OD.OrderID
+inner join Products as P
+on OD.ProductID=P.ProductID
+where P.ProductName in ('Gudbrandsdalsost','Lakkalikööri','Tourtière','Boston Crab Meat')
+--5. Empleados que no han vendido nunca “Chartreuse verte” ni “Ravioli Angelo”.
+>>>>>>> Stashed changes
 select FirstName,LastName from Employees
 except(
 select  E.FirstName,E.LastName from Employees as E
@@ -62,6 +77,7 @@ on O.CustomerID=C.CustomerID
 group by P.ProductName,C.Country
 having (COUNT(C.CustomerID)>1)
 
+<<<<<<< Updated upstream
 --9. Total de ventas (US$) en cada paï¿½s cada aï¿½o.
 select sum((OD.UnitPrice*OD.Quantity)-(OD.UnitPrice*OD.Quantity*OD.Discount))as [Total($)],year(O.OrderDate)as [AÃ±o de Venta],C.Country from [Order Details] as OD
 inner join Orders as O
@@ -98,6 +114,24 @@ select * from [Order Details]
 --12. Mejor cliente (el que mï¿½s nos compra) de cada paï¿½s.
 --13. Nï¿½mero de productos diferentes que nos compra cada cliente.
 --14. Clientes que nos compran mï¿½s de cinco productos diferentes.
+=======
+--9. Total de ventas (US$) en cada país cada año.
+select sum((UnitPrice*Quantity)-(UnitPrice*Quantity*Discount)),year(O.ShippedDate),C.Country from [Order Details] as OD
+inner join Orders as O
+on OD.OrderID=O.OrderID
+inner join Customers as C
+on O.CustomerID=C.CustomerID
+group by C.Country, O.ShippedDate
+
+--10. Producto superventas de cada año, indicando año, nombre del producto,
+--categoría y cifra total de ventas.
+select * from Products 
+--11. Cifra de ventas de cada producto en el año 97 y su aumento o disminución
+--respecto al año anterior en US $ y en %.
+--12. Mejor cliente (el que más nos compra) de cada país.
+--13. Número de productos diferentes que nos compra cada cliente.
+--14. Clientes que nos compran más de cinco productos diferentes.
+>>>>>>> Stashed changes
 --15. Vendedores que han vendido una mayor cantidad que la media en US $ en el
 --aï¿½o 97.
 --16. Empleados que hayan aumentado su cifra de ventas mï¿½s de un 10% entre dos
